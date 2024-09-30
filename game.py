b4129c4f0b00558ffd7fda740ea4490deeb1980c
SamGrigoli <122939984+SamGrigoli@users.noreply.github.com>
2024-09-30 15:04:45 -0400

    No changes

--- game.py
+++ game.py
@@ -1,16 +1,18 @@
 import pygame as pg
 
 
+
 class Game:
     def __init__(self):
         self.running = False
+        self.x, self.y = 500, 500  # Initial position of the circle
+        self.speed = 5  # Speed of the circle's movement
 
     def start(self):
         self.running = True
         pg.init()
         self.screen = pg.display.set_mode((1920, 1080))
         self.clock = pg.time.Clock()
-        pg.draw.circle(self.screen, "red", (500, 500), 40)
 
     def event(self):
         for e in pg.event.get():
@@ -18,10 +20,25 @@ class Game:
                 self.running = False
 
     def update(self):
-        pass
+        keys = pg.key.get_pressed()  # Get the state of all keys
+        if keys[pg.K_w]:  # Move up
+            self.y -= self.speed
+        if keys[pg.K_s]:  # Move down
+            self.y += self.speed
+        if keys[pg.K_a]:  # Move left
+            self.x -= self.speed
+        if keys[pg.K_d]:  # Move right
+            self.x += self.speed
 
     def render(self):
+        self.screen.fill((0, 0, 0))  # Clear the screen (black background)
+        pg.draw.circle(self.screen, "red", (self.x, self.y), 40)  # Draw the circle
         pg.display.flip()
 
     def clean(self):
         pg.quit()
+        
+        
+
+        
+
