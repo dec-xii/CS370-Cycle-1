1c251872d1ec655375e4ff33a96a19da5e3c7b72
dec-xii <declan0wayman@gmail.com>
2024-09-29 20:50:08 -0400

    Sprites class accepts a function for movement

--- game.py
+++ game.py
@@ -1,4 +1,11 @@
 import pygame as pg
+import sprites
+
+
+def movement(x):
+    x.rect[0] = x.rect[0] + 1
+    x.rect[1] = x.rect[1] + 1
+    return x
 
 
 class Game:
@@ -10,7 +17,10 @@ class Game:
         pg.init()
         self.screen = pg.display.set_mode((1920, 1080))
         self.clock = pg.time.Clock()
-        pg.draw.circle(self.screen, "red", (500, 500), 40)
+
+        self.player = sprites.Sprite(
+            "red", 100, 100, movement)
+        self.sprites = pg.sprite.RenderPlain(self.player)
 
     def event(self):
         for e in pg.event.get():
@@ -18,9 +28,11 @@ class Game:
                 self.running = False
 
     def update(self):
-        pass
+        self.player.update()
 
     def render(self):
+        self.screen.fill("black")
+        self.sprites.draw(self.screen)
         pg.display.flip()
 
     def clean(self):
